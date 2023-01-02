7e2fd09955b06486cda277cd921acba6a7edafa2
Lucas Carvalho <lucascarvalho34@gmail.com>
2023-01-02 15:54:59 -0300

    chore: figurint out insertion sort

---
+++ Algorithms/1 - InsertionSort/InsertionSort.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms._1___InsertionSort
+{
+    public static class InsertionSort
+    {
+        public static void SortByInsertionSort(this List<int> input)
+        {
+            int i, key;
+
+            for (int j = 1; j < input.Count; j++)
+            {
+                key = input[j];
+
+                i = j - 1;
+
+                while (i > 0 && input[i] > key)
+                {
+                    input[i + 1] = input[i];
+                    i--;
+                }
+
+                input[i + 1] = key;
+            }
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms._1___InsertionSort;
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithms!");
+
+            var input = new List<int>() { 5, 2, 4, 6, 1, 3 };
+
+            input.SortByInsertionSort();
+
+            Console.ReadKey();
+        }
+    }
+}
